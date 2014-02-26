ab2141820e9a2ac7bcb1374857d4e349bc9fdd67
Arturo Sekijima <arturo@MacBook-Pro-de-Arturo.local>
2014-02-26 17:27:34 -0300

    Mudei muito

--- Navigation/Navigation/LetraAViewController.m
+++ Navigation/Navigation/LetraAViewController.m
@@ -18,7 +18,7 @@ static int cont = 0;
 -(void) viewDidLoad {
     [super viewDidLoad];
     
-    Item *it = [[[ItemStore getInstancia]todosItens]objectAtIndex:cont++];
+    Item *it = [[[ItemStore getInstancia]todosItens]objectAtIndex:cont];
     
     
     
@@ -28,8 +28,15 @@ static int cont = 0;
                              initWithBarButtonSystemItem:UIBarButtonSystemItemFastForward target:self action:@selector(next:)];
     self.navigationItem.rightBarButtonItem=next;
     
-    UIButton *botao = [UIButton
-                                        buttonWithType:UIButtonTypeSystem];
+    
+    if(cont != 0){
+        UIBarButtonItem *previous = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemRewind target:self action:@selector(previous:)];
+        //UIBarButtonItem *previous = [[UIBarButtonItem alloc] initWithTitle:self.title style:UIBarButtonItemStylePlain target:self action:@selector(previous:)];
+        //self.navigationItem.backBarButtonItem = previous;
+        self.navigationItem.leftBarButtonItem = previous;
+    }
+    
+    UIButton *botao = [UIButton buttonWithType:UIButtonTypeSystem];
     [botao
      setTitle:@"Mostre uma palavra, uma figura e leia a palavra ao apertar um botao"
      forState:UIControlStateNormal];
@@ -38,16 +45,23 @@ static int cont = 0;
     
     [self.view addSubview:botao];
     
- 
+    
+}
+
+-(void) previous: (id) sender{
+    
+    [self.navigationController popViewControllerAnimated:YES];
+    cont--;
+    
 }
 
 -(void)next:(id)sender {
+    cont++;
     LetraAViewController *proximo = [[LetraAViewController alloc]
-                                              initWithNibName:nil
-                                            bundle:NULL];
+                                     initWithNibName:nil
+                                     bundle:NULL];
     [self.navigationController pushViewController:proximo
                                          animated:YES];
-    
 }
 
 
